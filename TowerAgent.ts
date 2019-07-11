41b3753bbc785cd0f6ce72b6c32ae741d6b92fde
Jupiter Bakakeu <jupiterbak2002@yahoo.fr>
2019-07-11 20:02:58 +0200

    Demo FATH

--- TowerAgent.ts
+++ TowerAgent.ts
@@ -38,43 +38,24 @@ import { DataPointValue, MindConnectAgent, MindsphereStandardEvent, retry, TimeS
                 for (let i = 0; i < 9; i++) { // Iterator Cities 
                     const values: DataPointValue[] = [
                         // Tower Description
-                        { "dataPointId": "1552817705488", "qualityCode": "0", "value": TowerNames[i].toString() },
-                        { "dataPointId": "1552817678788", "qualityCode": "0", "value": consumerNames[i].toString() },
-                        { "dataPointId": "1552817720914", "qualityCode": "0", "value": Latitudes[i].toString() },
-                        { "dataPointId": "1552817735881", "qualityCode": "0", "value": Longitudes[i].toString() },
+                        { "dataPointId": "1562865940567", "qualityCode": "0", "value": TowerNames[i].toString() },
+                        { "dataPointId": "1562865959864", "qualityCode": "0", "value": consumerNames[i].toString() },
+                        { "dataPointId": "1562865972587", "qualityCode": "0", "value": Latitudes[i].toString() },
+                        { "dataPointId": "1562865985067", "qualityCode": "0", "value": Longitudes[i].toString() },
                         // Article Names
-                        { "dataPointId": "1552817768009", "qualityCode": "0", "value": ArticleNames[0].toString() },
-                        { "dataPointId": "1552817790127", "qualityCode": "0", "value": ArticleNames[1].toString() },
-                        { "dataPointId": "1552817800164", "qualityCode": "0", "value": ArticleNames[2].toString() },
-                        { "dataPointId": "1552817812293", "qualityCode": "0", "value": ArticleNames[3].toString() },
-                        { "dataPointId": "1552817822593", "qualityCode": "0", "value": ArticleNames[4].toString() },
-                        { "dataPointId": "1552817832560", "qualityCode": "0", "value": ArticleNames[0].toString() },
-                        { "dataPointId": "1552817842599", "qualityCode": "0", "value": ArticleNames[1].toString() },
-                        { "dataPointId": "1552817856833", "qualityCode": "0", "value": ArticleNames[2].toString() },
-                        { "dataPointId": "1552817869593", "qualityCode": "0", "value": ArticleNames[3].toString() },
-                        { "dataPointId": "1552817881796", "qualityCode": "0", "value": ArticleNames[4].toString() },
+                        { "dataPointId": "1562866010556", "qualityCode": "0", "value": ArticleNames[0].toString() },
+                        { "dataPointId": "1562866023819", "qualityCode": "0", "value": ArticleNames[1].toString() },
+                        { "dataPointId": "1562866038103", "qualityCode": "0", "value": ArticleNames[2].toString() },
+
                         // ProducerNames
-                        { "dataPointId": "1552817918910", "qualityCode": "0", "value": ProducerNames[0].toString() },
-                        { "dataPointId": "1552817935346", "qualityCode": "0", "value": ProducerNames[1].toString() },
-                        { "dataPointId": "1552817944742", "qualityCode": "0", "value": ProducerNames[2].toString() },
-                        { "dataPointId": "1552817957983", "qualityCode": "0", "value": ProducerNames[3].toString() },
-                        { "dataPointId": "1552817968441", "qualityCode": "0", "value": ProducerNames[4].toString() },
-                        { "dataPointId": "1552817977966", "qualityCode": "0", "value": ProducerNames[0].toString() },
-                        { "dataPointId": "1552817988406", "qualityCode": "0", "value": ProducerNames[1].toString() },
-                        { "dataPointId": "1552817998231", "qualityCode": "0", "value": ProducerNames[2].toString() },
-                        { "dataPointId": "1552818008555", "qualityCode": "0", "value": ProducerNames[3].toString() },
-                        { "dataPointId": "1552818019431", "qualityCode": "0", "value": ProducerNames[4].toString() },
-                        // Aticle Quantities
-                        { "dataPointId": "1552818057789", "qualityCode": "0", "value": (Math.floor ( (Math.random() * 10) + 50)).toString() },
-                        { "dataPointId": "1552818074945", "qualityCode": "0", "value": (Math.floor ( (Math.random() * 30) + 20)).toString() },
-                        { "dataPointId": "1552818087398", "qualityCode": "0", "value": (Math.floor ( (Math.random() * 10) + 100)).toString() },
-                        { "dataPointId": "1552818097532", "qualityCode": "0", "value": (Math.floor ( (Math.random() * 50) + 20)).toString() },
-                        { "dataPointId": "1552818106409", "qualityCode": "0", "value": (Math.floor ( (Math.random() * 10) + 40)).toString() },
-                        { "dataPointId": "1552818115642", "qualityCode": "0", "value": (Math.floor ( (Math.random() * 5) + 5)).toString() },
-                        { "dataPointId": "1552818126030", "qualityCode": "0", "value": (Math.floor ( (Math.random() * 10) + 20)).toString() },
-                        { "dataPointId": "1552818137921", "qualityCode": "0", "value": (Math.floor ( (Math.random() * 50) + 5)).toString() },
-                        { "dataPointId": "1552818147861", "qualityCode": "0", "value": (Math.floor ( (Math.random() * 30) )).toString() },
-                        { "dataPointId": "1552818159889", "qualityCode": "0", "value": (Math.floor ( (Math.random() * 4))).toString() }
+                        { "dataPointId": "1562866052870", "qualityCode": "0", "value": ProducerNames[0].toString() },
+                        { "dataPointId": "1562866065342", "qualityCode": "0", "value": ProducerNames[1].toString() },
+                        { "dataPointId": "1562866074642", "qualityCode": "0", "value": ProducerNames[2].toString() },
+
+                        // ArticleQuantities
+                        { "dataPointId": "1562866095876", "qualityCode": "0", "value": (Math.floor ( (Math.random() * 10) + 50)).toString() },
+                        { "dataPointId": "1562866114679", "qualityCode": "0", "value": (Math.floor ( (Math.random() * 30) + 20)).toString() },
+                        { "dataPointId": "1562866125742", "qualityCode": "0", "value": (Math.floor ( (Math.random() * 10) + 100)).toString() }
                     ];
                     
                     // same like above, you can also just call  await agent.PostData(values) if you don't want to retry the operation
